017534430be82eb8811bb3931b9cb5bea1a7d0b3
bbagnes <bbagnes@byupathway.edu>
2025-03-15 10:21:17 -0600

    validation

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,22 +1,22 @@
-const currentCond = document.querySelector('#current-cond');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+// const currentCond = document.querySelector('#current-cond');
+// const weatherIcon = document.querySelector('#weather-icon');
+// const captionDesc = document.querySelector('figcaption');
 
-const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat=41.74&lon=--111.84&units=metric&appid=b99fc432ed06addb16967685aa3d1016';
+// const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat=41.74&lon=--111.84&units=metric&appid=b99fc432ed06addb16967685aa3d1016';
 
-async function apiFetch() {
-    try {
-      const response = await fetch(weatherURL);
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data); // testing only
-        currentCond.innerHTML = displayResults(data); // uncomment when ready        
-      } else {
-          throw Error(await response.text());
-      }
-    } catch (error) {
-        console.log(error);
-    }
-  }
+// async function apiFetch() {
+//     try {
+//       const response = await fetch(weatherURL);
+//       if (response.ok) {
+//         const data = await response.json();
+//         console.log(data); // testing only
+//         currentCond.innerHTML = displayResults(data); // uncomment when ready        
+//       } else {
+//           throw Error(await response.text());
+//       }
+//     } catch (error) {
+//         console.log(error);
+//     }
+//   }
   
-  apiFetch();
+//   apiFetch();
