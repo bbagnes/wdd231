57b08799df6a26ae20da8d31f2b152360db204b9
bbagnes <bbagnes@byupathway.edu>
2025-03-21 22:33:55 -0600

    reqs

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -3,9 +3,43 @@ const hambutton = document.querySelector('#menu');
 const headerspan = document.querySelector('span');
 const headerimg = document.querySelector("img");
 
+
 hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
     headerspan.classList.toggle('show');
     headerimg.classList.toggle('show');
 });
+
+const homeLink = document.querySelector(".home");
+const directoryLink = document.querySelector(".chamber");
+const joinLink = document.querySelector('.join');
+const discoverLink = document.querySelector('.discover');
+
+
+let clearActive  = () => {
+	gridLink.classList.remove('active');
+	listLink.classList.remove('active');
+};
+
+homeLink.addEventListener("click", () => {
+	clearActive();	
+	homeLink.classList.add('active');
+});
+
+directoryLink.addEventListener("click", () => {
+	clearActive();		
+	directoryLink.classList.add('active');
+});
+
+joinLink.addEventListener("click", () => {
+	clearActive();		
+	joinLink.classList.add('active');
+});
+
+discoverLink.addEventListener("click", () => {
+	clearActive();		
+	discoverLink.classList.add('active');
+});
+
+homeLink.classList.add('active');
