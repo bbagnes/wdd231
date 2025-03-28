4fc14b3bcfab18d27d276a621ee948aa9ee79da2
bbagnes <bbagnes@byupathway.edu>
2025-03-27 21:25:54 -0600

    styling

--- chamber/scripts/membership-stats.js
+++ chamber/scripts/membership-stats.js
@@ -13,8 +13,8 @@ nonButton.addEventListener("click", () => {
     dialogBox.classList.add('nonprofit');
     dialogBoxText.innerHTML = `
     <h3>Non Profit Membership Level</h3>
-    <p><strong>Benefits:</strong>Listing on Website</p>
-    <p><strong>Cost:</strong>Free to applicable members</p>`;
+    <p><strong>Benefits:</strong> Listing on Website</p>
+    <p><strong>Cost:</strong> Free to qualified members</p>`;
 });
 
 bronzeButton.addEventListener("click", () => {
@@ -23,8 +23,8 @@ bronzeButton.addEventListener("click", () => {
     dialogBox.classList.add('bronze');
     dialogBoxText.innerHTML = `
         <h3>Bronze Membership Level</h3>
-        <p><strong>Benefits:</strong>Listing on Website</p>
-        <p><strong>Cost:</strong>$50.00 (annual charge)</p>`;
+        <p><strong>Benefits:</strong> Listing on Website</p>
+        <p><strong>Cost:</strong> $50.00 (annual charge)</p>`;
 });
 
 silverButton.addEventListener("click", () => {
@@ -33,8 +33,8 @@ silverButton.addEventListener("click", () => {
     dialogBox.classList.add('silver');
     dialogBoxText.innerHTML = `
         <h3>Silver Membership Level</h3>
-        <p><strong>Benefits:</strong>Listing on Directory Page, Spotlight on Home Page, booth at regular events</p>
-        <p><strong>Cost:</strong>$200.00 (annual charge)</p>`;
+        <p><strong>Benefits:</strong> Listing on Directory Page, Spotlight on Home Page, booth at regular events</p>
+        <p><strong>Cost:</strong> $200.00 (annual charge)</p>`;
 });
 
 goldButton.addEventListener("click", () => {
@@ -43,8 +43,8 @@ goldButton.addEventListener("click", () => {
     dialogBox.classList.add('gold');
     dialogBoxText.innerHTML = `
         <h3>Gold Membership Level</h3>
-        <p><strong>Benefits:</strong>Listing on Directory Page, Spotlight on Home Page, booth at regular & special events</p>
-        <p><strong>Cost:</strong>$500.00 (annual charge)</p>`;
+        <p><strong>Benefits:</strong> Listing on Directory Page, Spotlight on Home Page, booth at regular & special events</p>
+        <p><strong>Cost:</strong> $500.00 (annual charge)</p>`;
 });
 
 closeButton.addEventListener("click", () => {
