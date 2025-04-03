48708b8a8a27c72415dde317ff0e857f066f8ed8
bbagnes <bbagnes@byupathway.edu>
2025-04-02 21:49:50 -0600

    project reqs

---
+++ final project/scripts/siteplan.js
@@ -0,0 +1,3 @@
+let currentDate = new Date();
+document.getElementById("currentyear").innerHTML = currentDate.getFullYear();
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
