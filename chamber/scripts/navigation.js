0b7bc4c1c4a4b28d5319bbe7963a84ac85474ae5
bbagnes <bbagnes@byupathway.edu>
2025-04-03 14:03:44 -0600

    styling

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,14 +1,11 @@
 const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('#menu');
-const headerspan = document.querySelector('span');
-const headerimg = document.querySelector("img");
-
+const header = document.querySelector('h1');
 
 hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
-    headerspan.classList.toggle('show');
-    headerimg.classList.toggle('show');
+    header.classList.toggle('show');
 });
 
 
