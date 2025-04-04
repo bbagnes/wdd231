05eb2aac171593672f6869cee73c213a3ca3ad59
bbagnes <bbagnes@byupathway.edu>
2025-04-03 21:22:35 -0600

    reqs

--- chamber/scripts/visit-tracker.js
+++ chamber/scripts/visit-tracker.js
@@ -5,16 +5,18 @@ onload = () => {
     let visitDate = Number(window.localStorage.getItem("visit")) || 0;
     // console.log(thisDate);
     // console.log(visitDate);
-    let daysAway = ((thisDate - visitDate)/86400000);
+    let daysAway = (Math.trunc((thisDate - visitDate)/8640000))/10;
     // console.log(daysAway);
         
     if (visitDate === 0) {
          visit.innerHTML = `<br>Welcome! Let us know if you have any questions.`;
         
+        } else if (daysAway >= 2) {
+            visit.innerHTML = `<br>You last visited ${daysAway.toFixed(0)} days ago.`;
+        } else if (daysAway == 1) {
+            visit.innerHTML = `<br>You last visited ${daysAway.toFixed(0)} day ago.`;
         } else if (daysAway < 1) {            
             visit.innerHTML = `<br>Back so soon! Awesome!`;
-        } else if (daysAway >= 1) {
-            visit.innerHTML = `<br>You last visited ${daysAway.toFixed(0)} days ago.`;
         }
     window.localStorage.setItem("visit", thisDate);       
     };
