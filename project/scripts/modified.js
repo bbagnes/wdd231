ab8b940266aaee061b3f1f841447cae5b7efec0e
bbagnes <bbagnes@byupathway.edu>
2025-04-09 11:47:44 -0600

    project reqs

--- project/scripts/modified.js
+++ project/scripts/modified.js
@@ -1,2 +1,2 @@
 let currentDate = new Date();
-document.getElementById('lastModified').innerHTML = `WDD 231 Class Project<br> Brigham Bagnes<br>©${currentDate.getFullYear()} Logan City Chamber of Commerce<br>Last Modified: ${document.lastModified}`;
+document.getElementById('lastModified').innerHTML = `WDD 231 Class Project<br> Brigham Bagnes<br>©${currentDate.getFullYear()} Seclusive Utah<br>Last Modified: ${document.lastModified}`;
