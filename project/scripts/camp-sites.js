09701eb1c4a08a8cfaad87b5c295c9ceb47fa2b2
bbagnes <bbagnes@byupathway.edu>
2025-04-09 10:48:53 -0600

    project reqs

--- project/scripts/camp-sites.js
+++ project/scripts/camp-sites.js
@@ -1,34 +1,67 @@
 const url = 'https://bbagnes.github.io/wdd231/project/data/camp-sites.json';
 const cards = document.querySelector('.sites div');
+let data = "";
 
 async function getData() {
     const response = await fetch(url);
     const data = await response.json();
-    // console.table(data.locations);
-    displayLocations(data.locations);
+    console.table(data.campSites);
+    onload = () => {
+	displaySites(data.campSites);
+	all.classList.add('active');
+    }
 }
 
-getData();
-    
+getData();    
 
-const displayLocations = (locations) => {
+const all = document.querySelector(".all");
+const sky = document.querySelector(".sky");
+const uncrowded = document.querySelector(".uncrowded");
+
+
+
+let clearActive  = () => {
+	all.classList.remove('active');
+	sky.classList.remove('active');
+	uncrowded.classList.remove('active');
+};
+
+all.addEventListener("click", () => {
+	clearActive();
+	displaySites(data.campSites);	
+	all.classList.add('active');
+});
+
+sky.addEventListener("click", () => {
+	clearActive();
+	displaySites(data.campSites.filter(site => data.campSites.darkSky === true));	
+	sky.classList.add('active');
+});
+
+uncrowded.addEventListener("click", () => {
+	clearActive();
+	displaySites(data.campSites.filter(bagi => data.campSites.avgVisitors < 3000));	
+	uncrowded.classList.add('active');
+});
+
+const displaySites = (sites) => {
         
-        locations.forEach((location) => {
+        sites.forEach((site) => {
             let card = document.createElement("section");
             let name = document.createElement('h2');
-            let address = document.createElement('address');
-            let description = document.createElement('p');
+            let location = document.createElement('p');
+            let activities = document.createElement('p');
             let photo = document.createElement('figure')
             let image = document.createElement('img');
             let button = document.createElement('button');
 
-            name.textContent = `${location.name}`;
-            address.innerHTML = `<strong>Address:</strong> ${location.address}`;
-            description.innerHTML = `<strong>Description:</strong> ${location.description}`;
+            name.textContent = `${site.siteName}`;
+            location.innerHTML = `<strong>Address:</strong> ${site.location}`;
+            activities.innerHTML = `<strong>Description:</strong> ${site.recreationOptions}`;
             button.textContent = `Learn more`;
 
-            image.setAttribute('src', location.imageurl);
-            image.setAttribute('alt', `Image of ${location.name}`);
+            image.setAttribute('src', site.images[0]);
+            image.setAttribute('alt', `Image of ${site.siteName}`);
             image.setAttribute('loading', 'lazy');
             image.setAttribute('width', '300');
             image.setAttribute('height', '200');
